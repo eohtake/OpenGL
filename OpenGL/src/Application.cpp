16532de2a95c18ed2cbe17830fdccd4140ccc03d
Eric Ohtake <ericevangelista@gmail.com>
2018-04-02 23:24:58 +0900

    Shader separation and index buffer

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,6 +1,45 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
+
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+				type = ShaderType::VERTEX;
+			else if (line.find("fragment") != std::string::npos)
+				type = ShaderType::FRAGMENT;
+		}
+		else
+		{
+			ss[(int)type] << line << '\n';
+		}
+	}
+	return { ss[0].str(), ss[1].str() };
+}
+
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
@@ -52,8 +91,6 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
-	
-	
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -65,53 +102,50 @@ int main(void)
 
 	/* Make the window's context current. */
 	glfwMakeContextCurrent(window);
-	
+
 	std::cout << glGetString(GL_RENDERER) << std::endl;
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	if (glewInit() != GLEW_OK) {
 		std::cout << "GLEW ERROR!\n";
-	} else { // If glew is placed correctly.
+	}
+	else { // If glew is placed correctly.
 		std::cout << "GLEW OK!\n";
 	}
-	
+
 	// Creating a buffer. Dados que serao colcoados dentro do buffer.
-	float positions[6] = {
-		-0.5f, -0.5f,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f
+	float positions[] = {
+		-0.5f, -0.5f, // vertex 0
+		 0.5f, -0.5f, // vertex 1
+		 0.5f,  0.5f, // vertex 2
+		-0.5f,  0.5f  // vertex 3
 	};
 
-	// Preparamos uma variavel para se tornar um vertex buffer object (VBO)
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	// Preparamos uma variavel para se tornar um vertex buffer object (VBO). Entretanto melhor utilizar um index buffer object
+	// para economizar memoria.
 	unsigned int buffer;
 	glGenBuffers(1, &buffer); // Gera 1 buffer e passa o endereco da variavel.
 	glBindBuffer(GL_ARRAY_BUFFER, buffer); // Ativa o buffer, indicando o tipo deste buffer e o proprio VBO.
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); // Coloca os dados dentro do VBO
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW); // Coloca os dados dentro do VBO
 	
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+	
+	/* INDEX BUFFER OBJECT - Renderizando utilizando indexes para economizar memoria*/
+    // Preparamos uma variavel para se tornar um index buffer object (IBO)
+	
+	unsigned int ibo;
+	glGenBuffers(1, &ibo); // Gera 1 buffer e passa o endereco da variavel.
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo); // Ativa o buffer, indicando o tipo deste buffer e o proprio index buffer object.
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW); // Coloca os dados dentro do buffer
 
-	std::string vertexShader = 
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n" 
-		"}\n";
-
-	std::string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) out vec4 color;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-		"}\n";
-
-	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
@@ -120,7 +154,10 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		/* Utilizar a linha abaixo, caso esteja renderizando direto, sem index buffer */
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
