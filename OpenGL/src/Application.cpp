3a6be3510bb398e5a9a4682d2eab40c009b95147
Eric Ohtake <ericevangelista@gmail.com>
2018-04-08 15:33:51 +0900

    Shader abstraction

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,91 +1,16 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
+
 #include "Renderer.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
-
-
-struct ShaderProgramSource
-{
-	std::string VertexSource;
-	std::string FragmentSource;
-};
-
-static ShaderProgramSource ParseShader(const std::string& filepath)
-{
-	std::ifstream stream(filepath);
-
-	enum class ShaderType
-	{
-		NONE = -1, VERTEX = 0, FRAGMENT = 1
-	};
-
-	std::string line;
-	std::stringstream ss[2];
-	ShaderType type = ShaderType::NONE;
-	while (getline(stream, line))
-	{
-		if (line.find("#shader") != std::string::npos)
-		{
-			if (line.find("vertex") != std::string::npos)
-				type = ShaderType::VERTEX;
-			else if (line.find("fragment") != std::string::npos)
-				type = ShaderType::FRAGMENT;
-		}
-		else
-		{
-			ss[(int)type] << line << '\n';
-		}
-	}
-	return { ss[0].str(), ss[1].str() };
-}
-
-
-static unsigned int CompileShader(unsigned int type, const std::string& source)
-{
-	unsigned int id = glCreateShader(type);
-	const char* src = source.c_str();
-	glShaderSource(id, 1, &src, nullptr);
-	glCompileShader(id);
-
-	// ERROR HANDLING
-	int result;
-	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-	if (result == GL_FALSE) 
-	{
-		int length;
-		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-		char* message = (char*)alloca(length * sizeof(char));
-		glGetShaderInfoLog(id, length, &length, message);
-		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl;
-		std::cout << message << std::endl;
-		glDeleteShader(id);
-		return 0;
-	}
-
-	return id;
-}
-
-static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
-{
-	unsigned int program = glCreateProgram();
-	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-
-	glAttachShader(program, vs);
-	glAttachShader(program, fs);
-	glLinkProgram(program);
-	glValidateProgram(program);
-
-	glDeleteShader(vs);
-	glDeleteShader(fs);
-
-	return program;
-}
+#include "VertexArray.h"
+#include "Shader.h"
 
 
 int main(void)
@@ -140,33 +65,32 @@ int main(void)
 		};
 
 		// Creates one vertex array object. The 1 literaly means create ONE vertex array object.
-		unsigned int vao;
-		GLCall(glGenVertexArrays(1, &vao));
-		GLCall(glBindVertexArray(vao));
+		//unsigned int vao;
+		//GLCall(glGenVertexArrays(1, &vao));
+		//GLCall(glBindVertexArray(vao));
 
+		//TODO: Planejamento Vertex Array Class
+		VertexArray va;
 		// Create a Vertex Buffer
 		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
-
-		GLCall(glEnableVertexAttribArray(0));
-		GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0)); // Primeiro parametro (0) aponta para o index do vao.
-
+				
+		//TODO: Planejamento buffer layout class
+		VertexBufferLayout layout;
+		layout.Push<float>(2);
+		va.AddBuffer(vb, layout);
+		
 		/* INDEX BUFFER OBJECT - Renderizando utilizando indexes para economizar memoria*/
 		IndexBuffer ib(indices, 6);
 
-		ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-		unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-		GLCall(glUseProgram(shader));
-
-		GLCall(int location = glGetUniformLocation(shader, "u_Color"));
-		ASSERT(location != -1);
-		GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
-
+		Shader shader("res/shaders/Basic.shader");
+		shader.Bind();
+		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+		
 		// Unbinding everything
-		GLCall(glUseProgram(0));
-		GLCall(glBindVertexArray(0));
-		GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
-
+		va.Unbind();
+		vb.Unbind();
+		ib.Unbind();
+		shader.Unbind();
 
 		float redChannel = 0.0f;
 		float redChannelIncrement = 0.05f;
@@ -175,12 +99,11 @@ int main(void)
 		while (!glfwWindowShouldClose(window))
 		{
 			/* Render here */
-			glClear(GL_COLOR_BUFFER_BIT);
+			GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
 			// bind the shader
-			GLCall(glUseProgram(shader));
-
-			GLCall(glUniform4f(location, redChannel, 0.3f, 0.8f, 1.0f));
+			shader.Bind();
+			shader.SetUniform4f("u_Color", redChannel, 0.3f, 0.8f, 1.0f);
 
 			// bind the buffer and re-enable the attributes if not using Vertex Array Object (VAO)
 			//GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer)); 
@@ -188,7 +111,9 @@ int main(void)
 			//GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
 			// Bind the vertex array object. This object is already bound with the attributes when glVertexAttribPointer was called.
-			GLCall(glBindVertexArray(vao));
+			//GLCall(glBindVertexArray(vao)); // TODO: Comentar esta linha quando a classe VertexArray for implementada
+			va.Bind(); // bind the vertex array
+			
 			ib.Bind(); // Bind the index buffer.
 
 			/* Utilizar a linha abaixo, caso esteja renderizando direto, sem index buffer */
@@ -210,8 +135,6 @@ int main(void)
 			/* Poll for and process events */
 			glfwPollEvents();
 		}
-
-		GLCall(glDeleteProgram(shader));
 	} // End of the big scope.
 
 	glfwTerminate();
--- OpenGL/src/VertexBuffer.cpp
+++ OpenGL/src/VertexBuffer.cpp
@@ -8,18 +8,17 @@ VertexBuffer::VertexBuffer(const void* data, unsigned int size)
 	GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));
 }
 
-
 VertexBuffer::~VertexBuffer()
 {
 	GLCall(glDeleteBuffers(1, &m_RendererID));
 }
 
-void VertexBuffer::Bind()
+void VertexBuffer::Bind() const
 {
 	GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
 }
 
-void VertexBuffer::Unbind()
+void VertexBuffer::Unbind() const
 {
 	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
 }
